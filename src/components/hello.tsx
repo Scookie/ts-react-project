85c0960bb1328d367cb0e68f5e48cae14fa3f65d
sanxing <sanxing@dtstack.com>
2020-03-14 22:53:13 +0800

    创建组件，定义Props和State类型

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,22 +1,59 @@
-import * as React from "react";
+import React,{ Component } from "react";
 
 export interface Props {
   name: string;
   enthusiasmLevel: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-  if(enthusiasmLevel <= 0){
-    throw new Error("you could be little more enthusiastic. :D");
+interface State {
+  currentEnthusiasm: number;
+}
+
+// function Hello({ name, enthusiasmLevel = 1 }: Props) {
+//   if(enthusiasmLevel <= 0){
+//     throw new Error("you could be little more enthusiastic. :D");
+//   }
+
+//   return(
+//     <div className="hello">
+//       <div className="greeting">
+//         Hello {name + getExclamationMask(enthusiasmLevel)}
+//       </div>
+//     </div>
+//   );
+// }
+
+class Hello extends Component<Props,State>{
+  constructor(props:Props){
+    super(props);
+    this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
   }
 
-  return(
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMask(enthusiasmLevel)}
+  onIncrement = () => {
+    this.setState({ currentEnthusiasm: this.state.currentEnthusiasm + 1});
+  }
+
+  onDecrement = () => {
+    this.setState({ currentEnthusiasm: this.state.currentEnthusiasm -1 })
+  }
+
+  render(){
+    const { name } = this.props;
+    const { currentEnthusiasm } = this.state;
+    if(currentEnthusiasm <= 0){
+      throw new Error("you could be little more enthusiastic. :D");
+    }
+  
+    return(
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMask(currentEnthusiasm)}
+        </div>
+        <button onClick={this.onIncrement}>+</button>
+        <button onClick={this.onDecrement}>-</button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import Hello from "./components/hello";
 
-ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={10} />, document.getElementById('root'));
+ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={3} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
