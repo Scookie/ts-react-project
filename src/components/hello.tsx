b902ed9beae438a7709316a9014a8451534b73d9
sanxing <sanxing@dtstack.com>
2020-03-19 22:53:05 +0800

    update hello.tsx add function

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -3,59 +3,65 @@ import "./hello.css"
 
 export interface Props {
   name: string;
-  enthusiasmLevel: number;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
 interface State {
   currentEnthusiasm: number;
 }
 
-// function Hello({ name, enthusiasmLevel = 1 }: Props) {
-//   if(enthusiasmLevel <= 0){
-//     throw new Error("you could be little more enthusiastic. :D");
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if(enthusiasmLevel <= 0){
+    throw new Error("you could be little more enthusiastic. :D");
+  }
+
+  return(
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMask(enthusiasmLevel)}    
+      </div>
+      <div>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+      </div>
+    </div>
+  );
+}
+
+// class Hello extends Component<Props,State>{
+//   constructor(props:Props){
+//     super(props);
+//     this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
 //   }
 
-//   return(
-//     <div className="hello">
-//       <div className="greeting">
-//         Hello {name + getExclamationMask(enthusiasmLevel)}
-//       </div>
-//     </div>
-//   );
-// }
+//   onIncrement = () => {
+//     this.setState({ currentEnthusiasm: this.state.currentEnthusiasm + 1});
+//   }
 
-class Hello extends Component<Props,State>{
-  constructor(props:Props){
-    super(props);
-    this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
-  }
+//   onDecrement = () => {
+//     this.setState({ currentEnthusiasm: this.state.currentEnthusiasm -1 })
+//   }
 
-  onIncrement = () => {
-    this.setState({ currentEnthusiasm: this.state.currentEnthusiasm + 1});
-  }
-
-  onDecrement = () => {
-    this.setState({ currentEnthusiasm: this.state.currentEnthusiasm -1 })
-  }
-
-  render(){
-    const { name } = this.props;
-    const { currentEnthusiasm } = this.state;
-    if(currentEnthusiasm <= 0){
-      throw new Error("you could be little more enthusiastic. :D");
-    }
+//   render(){
+//     const { name } = this.props;
+//     const { currentEnthusiasm } = this.state;
+//     if(currentEnthusiasm <= 0){
+//       throw new Error("you could be little more enthusiastic. :D");
+//     }
   
-    return(
-      <div className="hello">
-        <div className="greeting">
-          Hello {name + getExclamationMask(currentEnthusiasm)}
-        </div>
-        <button onClick={this.onIncrement}>+</button>
-        <button onClick={this.onDecrement}>-</button>
-      </div>
-    );
-  }
-}
+//     return(
+//       <div className="hello">
+//         <div className="greeting">
+//           Hello {name + getExclamationMask(currentEnthusiasm)}
+//         </div>
+//         <button onClick={this.onIncrement}>+</button>
+//         <button onClick={this.onDecrement}>-</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default Hello;
 
