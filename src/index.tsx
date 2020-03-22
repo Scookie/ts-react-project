4ecd46f15a646358de92e8fc3428994bb0801693
sanxing <sanxing@dtstack.com>
2020-03-22 22:04:06 +0800

    add file .env,update src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-import Hello from "./components/hello";
+// import Hello from "./components/hello";
 
-ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={3} />, document.getElementById('root'));
+// ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={3} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers";
+import { StoreState } from "./types";
+import Hello from "./containers/hello";
+import { Provider } from "react-redux";
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel:1,
+  languageName: "TypeScript"
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Hello />
+  </Provider>, 
+document.getElementById('root'));
